ca27bf2a50cf6fe129929151a7f8559ae48f9a38
mfourmy <mederic.fourmy@gmail.com>
2025-06-18 19:20:32 +0200

    display coloured point cloud

--- example.py
+++ example.py
@@ -4,6 +4,7 @@ import random
 import math
 
 # 3rd party
+import numpy as np
 import transforms3d
 import distinctipy
 import trimesh
@@ -14,8 +15,10 @@ from bop_toolkit_lib import misc
 from bop_toolkit_lib import dataset_params
 
 
+
 """
 Demo script displaying some bop ground truth data using the viser library.
+Sample a few views from a given scene, display camera poses, images, objects and pointclouds.
 
 Notes:
 - Use self corrective frame convention: T_13 = T_12 * T_23 
@@ -23,13 +26,37 @@ Notes:
 - Display single view estimates as meshes using 
 """
 
+
+def depth_im_to_xyz(depth_im, K):
+    """Converts a depth image to a xyz pointcloud.
+
+    :param depth_im: hxw ndarray with the input depth image, where depth_im[y, x]
+      is the Z coordinate of the 3D point [X, Y, Z] that projects to pixel [x, y],
+      or 0 if there is no such 3D point (this is a typical output of the
+      Kinect-like sensors).
+    :param K: 3x3 ndarray with an intrinsic camera matrix.
+    :return: hxwx3 ndarray with the XYZ coordinates for each pixel location from backprojected depth image.
+    """
+    # Only recomputed if depth_im.shape or K changes.
+    pre_Xs, pre_Ys = misc.Precomputer.precompute_lazy(depth_im, K)
+
+    xyz = np.stack([
+        np.multiply(pre_Xs, depth_im),
+        np.multiply(pre_Ys, depth_im),
+        depth_im.astype(np.float64),
+    ], axis=-1)
+
+    return xyz
+
+
 parser = argparse.ArgumentParser()
-parser.add_argument("--seed", type=int, default=0)
-parser.add_argument("--scene_id", type=int, default=48)
 parser.add_argument("--ds_name", type=str, default="ycbv")
 parser.add_argument("--ds_split", type=str, default="test")
+parser.add_argument("--seed", type=int, default=0)
+parser.add_argument("--scene_id", type=int, default=48)
 parser.add_argument("-N", "--number_of_views", type=int, default=4)
 parser.add_argument("--display_textured_mesh", action="store_true", default=False, help="textured mesh seem slower to load in viser")
+parser.add_argument("--display_poinclouds", action="store_true", default=False, help="Show pointclouds by backprojecting ground-truth depth")
 args = parser.parse_args()
 
 rng = random.Random(args.seed)
@@ -37,14 +64,12 @@ datasets_path = Path(config.datasets_path)
 # select a few views to represent single view estimates
 scene_id = args.scene_id
 
-ds_split = "test"
 targets_name = "test_targets_bop19.json"
 targets = inout.load_json(datasets_path / args.ds_name / targets_name)
 targets_org = misc.reorganize_targets(targets)
 im_ids = rng.sample(list(targets_org[scene_id].keys()), args.number_of_views)
-dp_split = dataset_params.get_split_params(datasets_path, args.ds_name, ds_split)
-model_type = None  # basic textured one
-model_type = "eval"  # smaller, no texture
+dp_split = dataset_params.get_split_params(datasets_path, args.ds_name, args.ds_split)
+model_type = None if args.display_textured_mesh else "eval"  
 dp_models = dataset_params.get_model_params(datasets_path, args.ds_name)
 
 # get paths to view files (gt, rgb, intrinsics)
@@ -85,6 +110,16 @@ for i, im_id in enumerate(im_ids):
         "color": colors[i],
     }
     viser_server.scene.add_camera_frustum(f"/camera_gt_{i}", **params_frust)
+
+    # point cloud from depth, expressed in camera frame
+    if args.display_poinclouds:
+        xyz = depth_im_to_xyz(depth, cam_K)
+        viser_server.scene.add_point_cloud(
+            f"/camera_gt_{i}/im_{im_id:06d}_pcd",
+            xyz.reshape(-1,3),  # (HxW, 3)
+            rgb.reshape(-1,3),  # (HxW, 3)
+            point_size=0.0005,
+        )
 
     # object poses in camera frame
     im_gts = scene_gt[im_id]
